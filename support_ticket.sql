ee24ff23e95d419f5e97b1961fb5b872915de1ef
sk-rezwan <skrezwan12@gmail.com>
2025-07-20 23:39:22 +0600

    db added

--- support_ticket.sql
+++ support_ticket.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Jun 11, 2025 at 07:27 PM
+-- Generation Time: Jul 20, 2025 at 05:37 PM
 -- Server version: 8.0.31
 -- PHP Version: 8.2.0
 
@@ -31,10 +31,20 @@ DROP TABLE IF EXISTS `branches`;
 CREATE TABLE IF NOT EXISTS `branches` (
   `id` bigint UNSIGNED NOT NULL AUTO_INCREMENT,
   `name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `branch_code` int DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `branches`
+--
+
+INSERT INTO `branches` (`id`, `name`, `branch_code`, `created_at`, `updated_at`) VALUES
+(1, 'Kuti', 1, NULL, NULL),
+(2, 'Dharkhar', 2, NULL, NULL),
+(3, 'Chargas', 3, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -49,11 +59,23 @@ CREATE TABLE IF NOT EXISTS `tickets` (
   `subject` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `description` text COLLATE utf8mb4_general_ci NOT NULL,
   `status` int DEFAULT '0',
+  `solved_by` varchar(333) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tickets`
+--
+
+INSERT INTO `tickets` (`id`, `user_id`, `subject`, `description`, `status`, `solved_by`, `created_at`, `updated_at`) VALUES
+(1, 6, 'test ticket', 'test description test description test description test description test description', 1, NULL, '2025-06-01 02:45:11', '2025-06-21 14:37:03'),
+(2, 2, 'Branch test create ticket 1', 'A problem description in IT, also known as a problem statement, clearly defines an issue within an IT system or application. It aims to pinpoint the problem, its impact, and the desired resolution, ensuring everyone involved understands the issue and works towards the same goal.', 1, '6', '2025-06-01 18:00:00', '2025-06-21 14:37:07'),
+(3, 3, 'test ticket issue', 'test ticket issuetest ticket issuetest ticket issuetest ticket issuetest ticket issue', 2, '1', '2025-06-03 14:37:40', '2025-06-21 14:37:48'),
+(4, 7, 'CDIP EYE Day end problem', 'Day end shows 16 June. Day back needed 15 June.', 0, '4', '2025-06-03 18:00:00', '2025-06-21 14:37:52'),
+(5, 8, 'Chargas Ianctive delete', 'Sir, SL. 155003 inactive delete please.', 2, '4', '2025-06-04 18:00:00', '2025-06-21 14:38:36');
 
 -- --------------------------------------------------------
 
@@ -75,18 +97,20 @@ CREATE TABLE IF NOT EXISTS `users` (
   `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=MyISAM AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `role`, `branch_id`, `created_at`, `updated_at`) VALUES
-(1, 'Admin User', 'admin@example.com', NULL, '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEaTn8LMpRyj5GH2a3O.Bf1uMeqG', NULL, 'admin', NULL, '2025-06-11 16:21:36', '2025-06-11 16:21:36'),
-(2, 'Branch One', 'branch1@example.com', NULL, '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEaTn8LMpRyj5GH2a3O.Bf1uMeqG', NULL, 'branch', 1, '2025-06-11 16:21:36', '2025-06-11 16:21:36'),
-(3, 'Branch Two', 'branch2@example.com', NULL, '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEaTn8LMpRyj5GH2a3O.Bf1uMeqG', NULL, 'branch', 2, '2025-06-11 16:21:36', '2025-06-11 16:21:36'),
-(4, 'Admin User', 'rezwanul@cdipbd.org', NULL, '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEaTn8LMpRyj5GH2a3O.Bf1uMeqG', NULL, 'admin', NULL, '2025-06-11 16:21:36', '2025-06-11 16:33:47'),
-(6, 'Admin User', 'admin@mail.com', NULL, '$2y$10$yvHbiZDU6APhR7k5WQsX1OvEUs3i/6Gf9e3IfyFlNQvWED9boYeSG', NULL, 'admin', NULL, '2025-06-11 16:21:36', '2025-06-11 19:07:56');
+(1, 'F M Nafis', 'admin@example.com', NULL, '$2y$10$.mFhM9dA9ByLG4tRqMjs0eLrl5QDvkCdHxbAV355ROvlMzwWSREqS', NULL, 'admin', NULL, '2025-06-13 02:28:57', '2025-06-20 18:28:56'),
+(2, 'Branch One', 'branch1@example.com', NULL, '$2y$10$aozX6iRlxCP1.5PznkORXOjNwlwrz1Jn/b8phEsOEk/sEsIKP0es6', NULL, 'branch', 1, '2025-06-13 02:28:57', '2025-06-13 02:28:57'),
+(3, 'Branch Two', 'branch2@example.com', NULL, '$2y$10$mh1QWO8kfx6dkG3nWoqM1.w0RDyHz4s/tJYpiZLedWngRJCX83Nw.', NULL, 'branch', 2, '2025-06-13 02:28:57', '2025-06-13 02:28:57'),
+(4, 'Rezwan', 'rezwanul@cdipbd.org', NULL, '$2y$10$opdPg8cNFG2WGWtNXKMqW.gmz8IgXQEivRzxMbnIHc/rkK/nvzd3S', NULL, 'admin', NULL, '2025-06-13 02:28:57', '2025-06-20 18:27:07'),
+(6, 'Dibya', 'admin@mail.com', NULL, '$2y$10$Z4k9sLDWiTiP77Xuzsdjm.Wi8MwEGj7y1TNlC.JOqYs0HjylT2Qhe', NULL, 'admin', NULL, '2025-06-13 02:28:57', '2025-06-20 18:27:21'),
+(7, 'Kuti', 'kuti001@cdipbd.org', NULL, '$2y$10$mh1QWO8kfx6dkG3nWoqM1.w0RDyHz4s/tJYpiZLedWngRJCX83Nw.', NULL, 'branch', 2, '2025-06-13 02:28:57', '2025-06-15 17:04:03'),
+(8, 'Chargas BM', 'chargas003@cdipbd.org', NULL, '$2y$10$mh1QWO8kfx6dkG3nWoqM1.w0RDyHz4s/tJYpiZLedWngRJCX83Nw.', NULL, 'branch', 3, '2025-06-13 02:28:57', '2025-06-15 17:04:03');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
